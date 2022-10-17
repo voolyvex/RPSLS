4109bdc12e41b68005bfb22f732059e1aaaa3c29
voolyvex <1joeknapp@gmail.com>
2022-10-17 15:38:06 -0500

    create main and welcome() function

---
+++ main.py
@@ -0,0 +1,15 @@
+# Step 1: Display Welcome message and the rules of the game
+# Step 2: Ask how many human players will be playing
+# Step 3: Display list of gestures and ask for gesture input
+# Step 4: display what gestures were picked by human or AI player
+# Step 5: Based on rules of the game Display who won or if it is a tie 
+# Step 6: Repeat process for additional rounds until there is a best-of-3 winner
+def welcome():
+    print("Welcome to Rock Paper Scissors Lizard Spock.\n")
+
+def main():
+    welcome()
+
+
+
+main()
