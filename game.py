5a6ea889f1eec3aa958070ee37da789732c5153f
voolyvex <1joeknapp@gmail.com>
2022-10-18 09:44:26 -0500

    create Game class which gets game type from game_settings

---
+++ game.py
@@ -0,0 +1,4 @@
+
+class Game:
+    def __init__(self, num_players):
+        self.game_type = num_players
--- main.py
+++ main.py
@@ -4,12 +4,32 @@
 # Step 4: display what gestures were picked by human or AI player
 # Step 5: Based on rules of the game Display who won or if it is a tie 
 # Step 6: Repeat process for additional rounds until there is a best-of-3 winner
+from game_settings import game_settings
+from game import Game
+
+
+gestures = ["Rock","Paper","Scissors","Lizard","Spock"]
+
 def welcome():
     print("Welcome to Rock Paper Scissors Lizard Spock.\n")
+    print("Each match will be best of three games")
+    print("Use the number keys to enter your selection\n")
+    print("Rock crushes Scissors")
+    print("Scissors cuts Paper")
+    print("Paper covers Rock")
+    print("Rock crushes Lizard")
+    print("Lizard poisons Spock")
+    print("Spock smashes Scissors")
+    print("Scissors decapitates Lizard")
+    print("Lizard eats Paper")
+    print("Paper disproves Spock")
+    print("Spock vaporizes Rock")
 
 def main():
     welcome()
+    new_game = Game(game_settings())
+    print(new_game.game_type)
+    print(gestures)
 
-
-
-main()
+if __name__ == "__main__":
+    main()
