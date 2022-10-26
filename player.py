dab621a01faa0f0946d57bbf6554c848d56a765b
voolyvex <1joeknapp@gmail.com>
2022-10-26 04:45:27 -0500

    ux improvement

--- player.py
+++ player.py
@@ -9,9 +9,9 @@ class Player:
         
     def get_gesture(self):
         if self.type == "Human":
-            pick_gesture = input("Choose (1) for Rock.\nChoose (2) for Paper.\nChoose (3) for Scissors.\nChoose (4) for Lizard.\nChoose (5) for Spock.\nChoose your gesture: ").strip()
+            pick_gesture = input("\nChoose (1) for Rock.\nChoose (2) for Paper.\nChoose (3) for Scissors.\nChoose (4) for Lizard.\nChoose (5) for Spock.\nChoose a gesture: ").strip()
             while pick_gesture not in ["1","2","3","4","5"]:
-                pick_gesture = input("Choose your gesture. (1) (2) (3) (4) or (5): ").strip()
+                pick_gesture = input("Choose a gesture. (1) (2) (3) (4) or (5): ").strip()
             pick = self.gesture_list[int(pick_gesture) - 1]
         else:
             pick = choice(self.gesture_list)
